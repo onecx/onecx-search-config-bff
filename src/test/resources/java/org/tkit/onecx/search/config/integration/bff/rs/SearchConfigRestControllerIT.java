a77d6c24c1cba7894c172936e46d83d52154c4b9
markuczy <martin.kuczynski@capgemini.com>
2024-08-01 16:19:17 +0200

    feat: init search-config bff

---
+++ src/test/resources/java/org/tkit/onecx/search/config/integration/bff/rs/SearchConfigRestControllerIT.java
@@ -0,0 +1,7 @@
+package org.tkit.onecx.search.config.bff.rs;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class SearchConfigRestControllerIT extends SearchConfigRestControllerTest {
+}
